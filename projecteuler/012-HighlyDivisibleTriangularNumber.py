0fc69a1a15fb300d2faa2ef323201cf96e8a5afb
Michael Clark <iiridayn@skylords.com>
2014-11-01 01:43:44 -0600

    Factoring out divisors function

--- projecteuler/012-HighlyDivisibleTriangularNumber.py
+++ projecteuler/012-HighlyDivisibleTriangularNumber.py
@@ -1,5 +1,6 @@
 import sys
 from math import ceil
+from euler import divisors
 
 def triangular():
     i = 1
@@ -8,15 +9,6 @@ def triangular():
         number += i
         i += 1
         yield number
-
-def divisors(num):
-    '''Naive function to calculate divisors of an integer'''
-    div = {1,num}
-    for i in range(2, ceil(pow(num,0.5))):
-        if not num % i:
-            div.add(i)
-            div.add(num//i)
-    return div
 
 target = int(sys.argv[1])
 for number in triangular():
--- projecteuler/euler.py
+++ projecteuler/euler.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 def primes():
     primes = [2,3]
     yield primes[0]
@@ -15,3 +17,12 @@ def primes():
             primes.append(i)
             yield i
         i += 2
+
+def divisors(num):
+    '''Naive function to calculate divisors of an integer'''
+    div = {1,num}
+    for i in range(2, ceil(pow(num,0.5))):
+        if not num % i:
+            div.add(i)
+            div.add(num//i)
+    return div
