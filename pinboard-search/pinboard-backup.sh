b760840bd2bd924bdb4953e7a7e503e493935dc6
Iiridayn <iiridayn@gmail.com>
2025-02-19 17:23:41 -0700

    Better file difference check - need to wait for another API error to catch those reliably

--- pinboard-search/pinboard-backup.sh
+++ pinboard-search/pinboard-backup.sh
@@ -3,7 +3,7 @@
 DATA_PATH=""
 AUTH_TOKEN=""
 curl "https://api.pinboard.in/v1/posts/all?auth_token=$AUTH_TOKEN&format=json" > /tmp/pinboard.json
-if [[ -f /tmp/pinboard.json && $(du /tmp/pinboard.json | cut -f 1) -gt $(du "$DATA_PATH"/pinboard.json | cut -f 1) ]]; then
+if [[ -f /tmp/pinboard.json && $(du -b /tmp/pinboard.json | cut -f 1) -gt $(du -b "$DATA_PATH"/pinboard.json | cut -f 1) ]]; then
 	mv /tmp/pinboard.json "$DATA_PATH"/pinboard.json
 	PID=$(systemctl show --property MainPID --value pinboard-search)
 	kill -s HUP "$PID"
